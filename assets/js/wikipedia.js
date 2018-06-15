9a8188008383484bcffebdfa6cd2619061c46050
Dragomir Yordanov <bad.stupid.monkey@gmail.com>
2018-06-15 10:10:17 +0200

    Fixed animation and creating dropdowns

--- assets/js/wikipedia.js
+++ assets/js/wikipedia.js
@@ -1,10 +1,21 @@
 
-
 document.addEventListener("DOMContentLoaded", function(){
 
-    document.querySelector('#menu-clicker').addEventListener('click', function(e){
-        document.querySelector('header').classList.toggle('active');
 
+    var headerElement = document.querySelector('header');
+
+    var headerHeight = headerElement.offsetHeight;
+    headerElement.style.maxHeight = headerHeight + 'px';
+    headerElement.classList.toggle('hidden');
+
+    document.querySelector('#menu-clicker').addEventListener('click', function(e){
+        document.querySelector('header').classList.toggle('hidden');
+    });
+
+
+    document.querySelectorAll('.dropdown').addEventListener('click', function(e){
+        console.log(this);
+        this.nextElementSibling.classList.toggle('hidden');
     });
 
 
