236ba53a078d766b0c57d62d28d61628e3950ef7
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2018-06-15 03:31:55 -0400

    working on the menu clicker transition

--- assets/js/wikipedia.js
+++ assets/js/wikipedia.js
@@ -1,16 +1,11 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
 
     document.querySelector('#menu-clicker').addEventListener('click', function(e){
-        toggleClass('header', 'active');
+        document.querySelector('header').classList.toggle('active');
+
     });
 
-
-});
-
-
-var toggleClass = (function(selector, classname){
-
-    let element = document.querySelector(selector);
-    return element.classList.toggle(classname);
 
 });
